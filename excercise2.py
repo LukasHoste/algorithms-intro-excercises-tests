e0ac659ca5d5b3671e4a4795c84bbf8c3ce89017
Matias Vereecke <matias.vereecke@student.vives.be>
2021-10-14 10:37:15 +0200

    refactor of sum of even LucasRow

--- excercise2.py
+++ excercise2.py
@@ -1,13 +1,30 @@
 def sumOfLucas(highestNumber):
-  previousNumber = 2
-  currentNumber = 1
-  sum = 1
-  while (currentNumber + previousNumber) < highestNumber:
-    temp = currentNumber
-    currentNumber = currentNumber + previousNumber
-    previousNumber = temp
-    if currentNumber % 2 != 0:
-      sum += currentNumber
-  return sum    
+  current =1
+  sum =2
+  while(current < highestNumber):
+    nextNumber = luc(current)
+    if nextNumber %2 ==0:
+      sum += nextNumber
+    current += 1
+  return sum
 
-print(sumOfLucas(4000000))
+  # previousNumber = 2
+  # currentNumber = 1
+  # sum = 1
+  # while (currentNumber + previousNumber) < highestNumber:
+  #   temp = currentNumber
+  #   currentNumber = currentNumber + previousNumber
+  #   previousNumber = temp
+  #   if currentNumber % 2 != 0:
+  #     sum += currentNumber
+  # return sum
+
+def luc(n):
+  if n == 0:
+    return 2
+  elif n == 1:
+    return 1
+  else:
+    return luc(n-1) + luc(n-2)
+
+print(sumOfLucas(4))
